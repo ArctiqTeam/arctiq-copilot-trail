51cb09c436e2d17cb6356644e0b47e8db02d3efa
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2024-11-29 14:55:00 -0500

    Clear methods for the workshop

--- labs/lab3-testing/Part 2/chessweb-cs/Models/Knight.cs
+++ labs/lab3-testing/Part 2/chessweb-cs/Models/Knight.cs
@@ -9,14 +9,7 @@ public class Knight : Piece
 
     public override bool IsValidMove(Position from, Position to, Board board)
     {
-        int rowDiff = Math.Abs(to.Row - from.Row);
-        int colDiff = Math.Abs(to.Column - from.Column);
-        
-        if (!((rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2)))
-            return false;
-            
-        // NOTE: Referencing a square that is out of bounds will cause an exception
-        var targetPiece = board.Squares[to.Row, to.Column];
-        return targetPiece == null || targetPiece.Color != Color;
+        // TODO: Implement using TDD
+        return true;
     }
 }
