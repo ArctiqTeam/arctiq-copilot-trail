ec2671ee72ab93f2afe2bbd27eaed72377cb9609
oxym <32470787+oxym@users.noreply.github.com>
2024-12-18 14:35:58 -0700

    remove code documentation in GameState.cs

--- chessweb-cs/Models/GameState.cs
+++ chessweb-cs/Models/GameState.cs
@@ -1,43 +1,14 @@
 using ChessWeb.Models;
 
-/// <summary>
-/// Represents the current state of the game.
-/// </summary>
 public class GameState
 {
-    /// <summary>
-    /// Gets or sets the board state.
-    /// </summary>
     public Board Board { get; set; } = new();
-
-    /// <summary>
-    /// Gets or sets the current turn.
-    /// </summary>
     public PieceColor CurrentTurn { get; private set; } = PieceColor.White;
-
-    /// <summary>
-    /// Gets or sets a value indicating whether the game is over.
-    /// </summary>
     public bool IsGameOver { get; set; }
-
-    /// <summary>
-    /// Gets or sets the last square a move was made from in the game.
-    /// </summary>
     public Position? LastMoveFrom { get; private set; }
-
-    /// <summary>
-    /// Gets or sets the last squarae a move was made to in the game.
-    /// </summary>
     public Position? LastMoveTo { get; private set; }
-
-    /// <summary>
-    /// Gets or sets the FEN representation of the board.
-    /// </summary>
     public string? FEN { get; set; }
     
-    /// <summary>
-    /// Initializes a new instance of the <see cref="GameState"/> class.
-    /// </summary>
     public GameState(string fen)
     {
         Board = new Board();
@@ -46,22 +17,12 @@ public class GameState
         IsGameOver = false;
     }
 
-    /// <summary>
-    /// Switches the turn to the other player.
-    /// </summary>
     public void SwitchTurn()
     {
         CurrentTurn = CurrentTurn == PieceColor.White ? 
             PieceColor.Black : PieceColor.White;
     }
 
-    /// <summary>
-    /// Attempts to move a piece from one position to another.
-    /// </summary>
-    /// <param name="from">The starting position of the piece.</param>
-    /// <param name="to">The destination position of the piece.</param>
-    /// <param name="message">An output message indicating the result of the move.</param>
-    /// <returns>True if the move is successful; otherwise, false.</returns>
     public bool MovePiece(Position from, Position to, out string message)
     {
         var piece = Board.Squares[from.Row, from.Column];
@@ -71,7 +32,6 @@ public class GameState
             return false;
         }
 
-        // Validate piece exists and belongs to current player
         if (piece.Color != CurrentTurn)
         {
             message = "It's not your turn";
@@ -79,7 +39,6 @@ public class GameState
         }
 
         bool isValidMove = piece.IsValidMove(from, to, Board);
-        //bool isValidMove = true;
 
         if (isValidMove)
         {
@@ -95,23 +54,17 @@ public class GameState
         return false;
     }
 
-    /// <summary>
-    /// Updates the last move made in the game.
-    /// </summary>
     public void UpdateLastMove(Position from, Position to)
     {
         LastMoveFrom = from;
         LastMoveTo = to;
     }
 
-    /// <summary>
-    /// Resets the game to its initial state.
-    /// </summary>
     public void Reset()
     {
         Board.SetPositionFromFen("start");
-        CurrentTurn = PieceColor.White; // Reset to white's turn
-        LastMoveFrom = null; // Clear last move
+        CurrentTurn = PieceColor.White;
+        LastMoveFrom = null;
         LastMoveTo = null;
         IsGameOver = false;
     }
